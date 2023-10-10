2accb0d876b1b77a7b39f88ae3cc82ab9dbbf0ec
Corina <ayecori3@gmail.com>
2023-10-10 17:03:41 -0300

    Codigo probado

--- _html.py
+++ _html.py
@@ -1,5 +1,7 @@
 import os
 from lxml import etree
+import xml.etree.ElementTree as ET
+from bs4 import BeautifulSoup
 
 class HTML:
     
@@ -11,56 +13,56 @@ class HTML:
     def open_html(self, encoding='utf-8'):
             
         if not self.path:
-            raise FileNotFoundError('Html File Missing')
+            raise FileNotFoundError('HTML File Missing')
             
         with open(self.path, encoding=encoding) as f:
-            self.code = f.read()
-            self._html = etree.HTML(self.code)
-
-    def add_tag(self, tag, tag_text, attr, attr_text, xpath=None):
             
+            self.code = f.read()
+            # print(self.code)
+            self._html = BeautifulSoup(self.code, 'html.parser')
+
+    def add_tag(self, tag, tag_text, attr, attr_text, xpath=None):        
             
         if not tag:
             raise Exception('Tag name undefined')
         
         if self._html is None:
-            raise FileNotFoundError('The HTML file is not open')
+            raise FileNotFoundError('HTML file is not open')
         
-        new_tag = etree.Element(tag) #crea la nueva etiqueta
-        new_tag.text = tag_text
-        new_tag.set(attr, attr_text) #setea el atributo
+        new_tag = self._html.new_tag(tag) #crea la nueva etiqueta
+        new_tag.string = tag_text #setea la leyenda de la etiqueta
+        new_tag[attr]=attr_text #setea el atributo
 
         if xpath: #si se proporciona la ubicacion
             
-            location = self._html.xpath(xpath) #busca el elemento xpath
-            #agrega la nueva etiqueta
-            etree.SubElement(location, new_tag)
+            location = self._html.select(xpath) #busca el elemento xpath
+            print(location)
+            location = location[0]
+            print(location)
+            #location.insert(0, new_tag)
+            location.append(new_tag)
+            # print(etree.tostring(location))
+            #etree.ElementTree.SubElement(location, new_tag) #agrega la nueva etiqueta
+            #etree.ElementTree.SubElement(location, new_tag)
 
         else: #sino, lo agrega al final
             self._html.append(new_tag)
        
         
     def save_html(self, path_save=None, decode='utf-8'):
-
         if self._html is None:
             raise FileNotFoundError('The HTML file is not open')
 
-        try:
-
-            if path_save is None:
-                path_save = self.path
+        if path_save is None:
+            path_save = self.path
                 
-            with open(path_save, 'a') as saved_html:
-                saved_html.write(self.to_string(decode))
-            print('Successfully Saved')        
-
-        except Exception as e:
-            print('Error')
-
-
+        with open(path_save, 'w', encoding=decode) as saved_html:
+            #saved_html.write(etree.tostring(self._html))
+            saved_html.write(self.to_string(decode))
+        print('Successfully Saved')    
+        
     def to_string(self, decode='utf-8'):
-        return etree.tostring(self._html, encoding=decode).decode(decode)
-
+        return self._html.prettify(decode).decode(decode)
 
 
     
